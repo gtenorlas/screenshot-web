0aa68555006aa4b6b7c68413f6a4a1f851b0f7a6
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-02-03 16:49:40 +0000

    Install dotenv and implement .env

--- screenshot.js
+++ screenshot.js
@@ -1,27 +1,26 @@
-const request = require('request-promise-native')
+const request = require("request-promise-native");
 
 // @param {String} token - String containing your API Key
 // @param {String} url - Encoded URI string container the URI you're targeting
 // @param {Integer} width - Integer indicating the width of your target render
 // @param {Integer} height - Integer indicating the height of your target render
 // @param {String} output - String specifying the output format, "image" or "json"
-var token = "1X4KTM6-9404RJA-QZ531EP-B8405CE";
-//var url = encodeURIComponent("https://google.com");
-var width = 1920;
-var height = 1080;
-var output = "json";
-
-// Construct the query params and URL
-
-// Call the API and save the screenshot
 const screenshot = (url) => {
+  require('dotenv').config();
 
+  // Construct the query params and URL
+  const {TOKEN} = process.env;
+  const width = 1920;
+  const height = 1080;
+  const output = "json";
   var query = "https://shot.screenshotapi.net/screenshot";
-  query += `?token=${token}&url=${url}&width=${width}&height=${height}&output=${output}`;
+  query += `?token=${TOKEN}&url=${url}&width=${width}&height=${height}&output=${output}`;
 
-  return request.get({url: query, encoding: 'binary'}, (err, response, body) => {
-    return body;
-  });
+  return request.get(
+    { url: query, encoding: "binary" },
+    (err, response, body) => {
+      return body;
+    }
+  );
 };
-//screenshot('https://google.com');
 module.exports = { screenshot };
